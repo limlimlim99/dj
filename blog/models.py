37b18a1cbc73e043bd192ae07e53d2132c7d33e0
limlimlim99 <limgh31842@gmail.com>
2023-02-10 11:14:17 +0900

    static 정적파일, landing, about_me, 카테고리 기능 추가

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 import os
 from django.db import models
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 # from markdownx.models import MarkdownxField
 # from markdownx.utils import markdown
 
@@ -14,28 +14,28 @@ from django.db import models
 #     def get_absolute_url(self):
 #         return f'/blog/tag/{self.slug}/'
         
-# class Category(models.Model):
-#     name = models.CharField(max_length=50, unique=True)
-#     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
 
-#     def get_absolute_url(self):
-#         return f'/blog/category/{self.slug}/'
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
 
-# class Meta:
-#     verbose_name_plural = 'categories'
+class Meta:
+    verbose_name_plural = 'categories'
 
 # Post는 기본 구조이다.
 class Post(models.Model):
-    # 장고 방식으로 형태 바꿔줌 (글 제목이 보일수 있게 해줌)
+    # 장고 방식으로 형태 바꿔줌
     title = models.CharField(max_length=30)
-    # hook_text = models.CharField(max_length=100, blank=True)
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
 
-    # head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
-    # file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
 
     # 생성해주자.
     # 현재의 시간을 자동으로 추가해주기.
@@ -46,10 +46,10 @@ class Post(models.Model):
 
     # # 작성자
     # # ForeignKey는 연결 시켜서 없애줄때 같이 없애기 위함.
-    # author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
     # # 카테고리
-    # category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
 
     # tags = models.ManyToManyField(Tag, blank=True)
 
@@ -66,12 +66,12 @@ class Post(models.Model):
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
 
-#     def get_file_name(self):
-#         return os.path.basename(self.file_upload.name)
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
     
-#     # 파일 확장자 얻는거
-#     def get_file_ext(self):
-#         return self.get_file_name().split('.')[-1]
+    # 파일 확장자 얻는거
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
 
 #     def get_content_markdown(self):
 #         return markdown(self.content)
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,8 @@
-# view는 함수방식, class방식 2개가 있다.
-# listviwe를 상속을 받아서 씀 
-
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Post
+from .models import Post, Category
+# view는 함수방식, class방식 2개가 있다.
+# listviwe를 상속을 받아서 씀 
 
 # listview 상속받기
 class PostList(ListView):
@@ -11,5 +10,37 @@ class PostList(ListView):
     # ordering은 순서이다. 최근에 온 것이 가장 먼저 뜨게 함
     ordering = '-pk'
 
+
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data()
+        context['categories']= Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
+
 class PostDetail(DetailView):
     model = Post
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data()
+        context['categories']= Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
+
+
+def category_page(request, slug):   # slug는 일반적으로 이미 얻은 데이터를 사용하여 유효한 url을 생성하는 방법
+    if slug == 'no category':
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
+    
+    return render(
+        request, 
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+            'category': category, 
+        } 
+    )
