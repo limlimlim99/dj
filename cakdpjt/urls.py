ffb21f2f6e14cf8bafc739a82a3840594294956a
limlimlim99 <limgh31842@gmail.com>
2023-02-13 02:01:27 +0000

    static 정적파일, landing, about_me, 카테고리 기능 추가_0210

--- cakdpjt/urls.py
+++ cakdpjt/urls.py
@@ -18,8 +18,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('', include('homepage.urls')), # 아무것도 없으면 홈페이지 url로 가라
+    path('markdownx/', include('markdownx.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
